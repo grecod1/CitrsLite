fa78ec5cc8d7e79f21183418375922f942ea1d46
Daniel Greco <Daniel.Greco@fdacs.gov>
2022-11-28 16:17:27 -0500

    Added all properties onto the Tree Model.

--- CitrsLite.Data/Models/Tree.cs
+++ CitrsLite.Data/Models/Tree.cs
@@ -9,9 +9,16 @@ namespace CitrsLite.Data.Models
     public class Tree
     {
         public int Id { get; set; }
+        public string Number { get; set; }
         public string VarietyClone { get; set; }
 
         public string Description { get; set; }
+
+        public bool IsActive { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModificationDate { get; set; }
 
     }
 }
