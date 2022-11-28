034d58acb5fccc8805e6f9aebfa71caf2783f543
Don Vu <Don.Vu@fdacs.gov>
2022-11-28 16:22:40 -0500

    Added fields to Participant Model

--- CitrsLite.Data/Models/Participant.cs
+++ CitrsLite.Data/Models/Participant.cs
@@ -10,6 +10,20 @@ namespace CitrsLite.Data.Models
     {
         public int Id { get; set; }
 
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+
+        public bool IsActive { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string ModifiedBy { get; set;}
+        public DateTime ModificationDate { get; set;}
 
     }
 }
