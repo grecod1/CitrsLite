d7c0245fc9c1c68d13f391a100c168f747d5d033
Don Vu <Don.Vu@fdacs.gov>
2022-11-29 10:16:18 -0500

    Add Budwood model

---
+++ CitrsLite.Data/Models/Budwood.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CitrsLite.Data.Models
+{
+    internal class Budwood
+    {
+        public int Quantity { get; set; }
+        public string Name { get; set; }
+
+        public int NumberBudded { get; set; }
+        public int NumberGrafted { get; set; }
+
+        public bool IsActive { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModificationDate { get; set; }
+    }
+}
