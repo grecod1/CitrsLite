dda0b25fad45ea06efcee03183b42998bfa5617c
Don Vu <Don.Vu@fdacs.gov>
2022-11-29 14:22:20 -0500

    Add data validation annotations to Budwood model

--- CitrsLite.Data/Models/Budwood.cs
+++ CitrsLite.Data/Models/Budwood.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,15 +9,21 @@ namespace CitrsLite.Data.Models
 {
     public class Budwood
     {
+        [Required]
+        public int Id { get; set; }
+        [Required]
         public int Quantity { get; set; }
+        [Required]
         public string Name { get; set; }
 
-        public int NumberBudded { get; set; }
-        public int NumberGrafted { get; set; }
+        public int? NumberBudded { get; set; }
+        public int? NumberGrafted { get; set; }
 
         public bool IsActive { get; set; }
+        [Required]
         public string CreatedBy { get; set; }
         public DateTime CreationDate { get; set; }
+        [Required]
         public string ModifiedBy { get; set; }
         public DateTime ModificationDate { get; set; }
     }
