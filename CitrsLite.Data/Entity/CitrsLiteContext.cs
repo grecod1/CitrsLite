81e35595ec38ff044e7d19378d8b116d6b2c84f2
Don Vu <Don.Vu@fdacs.gov>
2022-11-29 14:53:17 -0500

    Add Budwoods to Context, migration scripts

--- CitrsLite.Data/Entity/CitrsLiteContext.cs
+++ CitrsLite.Data/Entity/CitrsLiteContext.cs
@@ -15,7 +15,7 @@ namespace CitrsLite.Data.Entity
         public DbSet<VarietyClone> VarietyClones { get; set; }
         public DbSet<Tree> Trees { get; set; }
         public DbSet<TreeType> TreeTypes { get; set; }
-
+        public DbSet<Budwood> Budwoods { get; set; }
 
         public CitrsLiteContext(DbContextOptions<CitrsLiteContext> options) : base(options) { }
         
