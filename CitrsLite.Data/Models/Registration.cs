0f3f6e4c9ee25f87ac3e9dfeffea5adde5551c09
Daniel Greco <Daniel.Greco@fdacs.gov>
2022-11-29 13:53:02 -0500

    Updated Registration Model.

--- CitrsLite.Data/Models/Registration.cs
+++ CitrsLite.Data/Models/Registration.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace CitrsLite.Data.Models
     {
         public int Id { get; set; }
 
+        [Required]
         public string RegistrationNumber { get; set; }
 
         public string Description { get; set; }
