734cc7f221a4e9a8a4521a38e4f21006731913a3
Daniel Greco <Daniel.Greco@fdacs.gov>
2022-11-29 16:25:49 -0500

    Added in the Tree Location Model

---
+++ CitrsLite.Data/Models/TreeLocation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CitrsLite.Data.Models
+{
+    public class TreeLocation
+    {
+        public int Id { get; set; }
+        public string BlockNumber { get; set; }
+        public string Row { get; set; }
+        public DateTime DatePlanted { get; set; }
+        public DateTime? DateRemoved { get; set; }
+
+        public bool IsActive { get; set; }
+        [Required]
+        public string CreatedBy { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        [Required]
+        public string ModifiedBy { get; set; }
+        public DateTime ModificationDate { get; set; }
+    }
+}
