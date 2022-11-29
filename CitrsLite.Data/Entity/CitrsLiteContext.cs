de8c00432384b98da1ce3abf5f7528aae4d288c3
Daniel Greco <Daniel.Greco@fdacs.gov>
2022-11-29 11:44:36 -0500

    Created the entity object.

---
+++ CitrsLite.Data/Entity/CitrsLiteContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CitrsLite.Data.Entity
+{
+    public class CitrsLiteContext : DbContext
+    {
+
+
+        public CitrsLiteContext(DbContextOptions<CitrsLiteContext> options) : base(options) { }
+
+        public CitrsLiteContext(string connectionString)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "";
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+        
+    }
+}
