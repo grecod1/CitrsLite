79cf4a0d66ce5f995401ec6f2293ec3431907844
Daniel Greco <Daniel.Greco@fdacs.gov>
2022-11-29 16:29:15 -0500

    Added in the tree location and tree relationship.

--- CitrsLite.Data/Models/TreeLocation.cs
+++ CitrsLite.Data/Models/TreeLocation.cs
@@ -14,6 +14,9 @@ namespace CitrsLite.Data.Models
 
         public int ParticipantId { get; set; }
         public Participant Participant { get; set; }
+
+        public int TreeId { get; set; }
+        public Tree Tree { get; set; }
         public string BlockNumber { get; set; }
         public string Row { get; set; }
         public DateTime DatePlanted { get; set; }
