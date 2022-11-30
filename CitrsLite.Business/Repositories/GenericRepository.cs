3fa25625cf102e060cf276c70816795438a2c2c7
Daniel Greco <Daniel.Greco@fdacs.gov>
2022-11-30 12:24:50 -0500

    Created the buiness layer and entity object.

---
+++ CitrsLite.Business/Repositories/GenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using CitrsLite.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CitrsLite.Business.Repositories
+{
+    /// <summary>
+    /// Generic Repository Class
+    /// </summary>
+    /// <typeparam name="T">Entity Object</typeparam>
+    internal class GenericRepository<T> where T : class
+    {
+        internal CitrsLiteContext _dbContext;
+        internal DbSet<T> _dbSet;
+
+    }
+}
