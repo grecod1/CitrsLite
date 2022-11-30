a7f8d6ade75ee45ba777d1078b40f9f861955b50
Daniel Greco <Daniel.Greco@fdacs.gov>
2022-11-30 13:29:02 -0500

    Added more content onto the Interface.

--- CitrsLite.Business/Repositories/Interfaces/IGenericRepository.cs
+++ CitrsLite.Business/Repositories/Interfaces/IGenericRepository.cs
@@ -8,6 +8,10 @@ using System.Threading.Tasks;
 
 namespace CitrsLite.Business.Repositories.Interfaces
 {
+    /// <summary>
+    /// Generic repository,
+    /// </summary>
+    /// <typeparam name="T">Entity Model</typeparam>
     internal interface IGenericRepository<T> where T : class
     {
         /// <summary>
@@ -22,10 +26,28 @@ namespace CitrsLite.Business.Repositories.Interfaces
         /// <summary>
         /// Return a list of models from the database.
         /// </summary>
-        /// <param name="predicate">Condition</param>
+        /// <param name="predicate">The filter condition</param>
         /// <param name="includedProperties">Included properties</param>
         /// <returns>List of models.</returns>
         public Task<IList<T>> GetListAsync(Expression<Func<T, bool>>? predicate = null,
             params string[] includedProperties);
+
+        /// <summary>
+        /// Creates a new Model in the Database.
+        /// </summary>
+        /// <param name="t">Model being Created</param>
+        public void Create(T t);
+
+        /// <summary>
+        /// Updates an existing Model.
+        /// </summary>
+        /// <param name="t">Pre-existing Model in the Database</param>
+        public void Edit(T t);
+
+        /// <summary>
+        /// Remove Entry from Database.
+        /// </summary>
+        /// <param name="t">Entry in the Database you want to Remove</param>
+        public void Remove(T t);
     }
 }
