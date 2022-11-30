8dfef849ceed45446e2413f11986edd6b6b5a478
DrakeDawkinsFDACS <118310282+DrakeDawkinsFDACS@users.noreply.github.com>
2022-11-30 12:43:30 -0500

    Added CRUD actions to GenericRepository.

--- CitrsLite.Business/Repositories/GenericRepository.cs
+++ CitrsLite.Business/Repositories/GenericRepository.cs
@@ -18,5 +18,43 @@ namespace CitrsLite.Business.Repositories
         internal CitrsLiteContext _dbContext;
         internal DbSet<T> _dbSet;
 
+        public GenericRepository(CitrsLiteContext, dbContext)
+        {
+            this._dbContext = _dbContext;
+            this._dbSet = _dbContext.Set<T>();
+
+            protected void Dispose()
+            {
+                _dbContext.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Creates a new Model in the Database.
+        /// </summary>
+        /// <param name="t">Model being Created</param>
+        public virtual void Create(T t)
+        {
+            _dbSet.Add(t);
+        }
+
+        /// <summary>
+        /// Updates an existing Model.
+        /// </summary>
+        /// <param name="t">Pre-existing Model in the Database</param>
+        public virtual void Edit (T t)
+        {
+            _dbSet.Attach(t);
+            _dbContext.Entry(t).State = EntityState.Modified;
+        }
+
+        /// <summary>
+        /// Remove Entry from Database.
+        /// </summary>
+        /// <param name="t">Entry in the Database you want to Remove</param>
+        public virtual void Remove(T t)
+        {
+            _dbSet.Remove(t);
+        }
     }
 }
