ab2bb5df8e2c6cef154a6decad5ae8f8b0ef6c8f
DrakeDawkinsFDACS <118310282+DrakeDawkinsFDACS@users.noreply.github.com>
2022-12-01 16:01:47 -0500

    Created folders and files for VarietyCloneForm. Created the VarietyCloneFormViewModel.

---
+++ CitrsLite.Business/ViewModels/VarietyCloneViewModels/VarietyCloneFormViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CitrsLite.Business.ViewModels.VarietyCloneViewModels
+{
+    public class VarietyCloneFormViewModel
+    {
+        /// <summary>
+        /// This is the Id of the Variety Clone.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// This is the Name of the Variety Clone and is Required.
+        /// </summary>
+        [Required]
+        [Display(Name= "Variety Name")]
+        public string Name { get; set; }       
+
+        /// <summary>
+        /// This is an optional Description of the Variety Clone.
+        /// </summary>
+        public string? Description { get; set; }
+
+        [Display(Name = "Status")]
+        /// <summary>
+        /// This boolean determines the Status of the Variety Clone.
+        /// True = Active, False = Inactive.
+        /// </summary>
+        public bool IsActive { get; set; }
+    }
+}
