83d4a2745c216a1fa83db15572a6fa4a19cee2e6
Daniel Greco <Daniel.Greco@fdacs.gov>
2023-01-23 15:56:43 -0500

    Seperated the Class and the Blazor Component in the Participant Form. Made the form have a card background.

---
+++ CitrsLite/Pages/Participants/ParticipantForm.razor.cs
@@ -0,0 +1,40 @@
+﻿using CitrsLite.Business.ViewModels.ParticipantViewModels;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components;
+
+namespace CitrsLite.Pages.Participants
+{
+    public partial class ParticipantForm
+    {
+        private String Message = String.Empty;
+
+
+
+        public void CreateParticipant(EditContext context)
+        {
+            var valid = context.Validate();
+
+            if (valid)
+            {
+                participantService.Create(Model);
+            }
+            else
+            {
+                Message = "Error";
+            }
+
+        }
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            var authState = await authenticationStateProvider.GetAuthenticationStateAsync();
+
+            Model.UserName = authState.User.Identity?.Name ?? "Unknown user";
+        }
+
+        [Inject]
+        public ParticipantFormViewModel Model { get; set; }
+
+    }
+}
