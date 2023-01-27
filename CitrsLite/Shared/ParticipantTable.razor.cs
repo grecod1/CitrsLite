007564eec5eddb4dc10fbee8c076879339856d88
Daniel Greco <Daniel.Greco@fdacs.gov>
2023-01-27 10:11:46 -0500

    Created a filter function and a seperate C# class for the Participant Table partial view.

---
+++ CitrsLite/Shared/ParticipantTable.razor.cs
@@ -0,0 +1,51 @@
+﻿using CitrsLite.Data.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace CitrsLite.Shared
+{
+    public partial class ParticipantTable
+    {
+        [Parameter]
+        public IEnumerable<Participant> Participants { get; set; }
+
+        private string search;
+
+        private bool filterFunc1(Participant participant) => filterFunc(participant, search);
+
+        private bool filterFunc(Participant participant, string searchString)
+        {
+            if (string.IsNullOrWhiteSpace(searchString))
+            {
+                return true;
+            }
+
+            if (participant.Name.Contains(searchString, StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+
+            if (participant.Type.Contains(searchString, StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+
+            if (participant.City.Contains(searchString, StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+
+            if (participant.PhoneNumber.Contains(searchString, StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+
+            if (participant.State.Contains(searchString, StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+    }
+}
