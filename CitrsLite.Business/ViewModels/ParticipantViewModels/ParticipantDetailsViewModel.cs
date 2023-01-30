7ca99b957172e478e68d6dddb15263294870b8bf
Daniel Greco <Daniel.Greco@fdacs.gov>
2023-01-30 12:54:29 -0500

    Finished the Participant Details View Model and gave the proper URL to the participant index .

--- CitrsLite.Business/ViewModels/ParticipantViewModels/ParticipantDetailsViewModel.cs
+++ CitrsLite.Business/ViewModels/ParticipantViewModels/ParticipantDetailsViewModel.cs
@@ -9,5 +9,9 @@ namespace CitrsLite.Business.ViewModels.ParticipantViewModels
     public class ParticipantDetailsViewModel
     {
         public int Id { get; set; }
+        public string? Type { get; set; }
+        public string? Name { get; set; }
+        public string? City { get; set; }
+        public string? PhoneNumber { get; set; }
     }
 }
