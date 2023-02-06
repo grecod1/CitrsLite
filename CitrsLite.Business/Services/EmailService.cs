9e284646c7c355d13dadd15727634c273a988696
Daniel Greco <Daniel.Greco@fdacs.gov>
2023-02-06 15:15:19 -0500

    Created the Email Service Class

---
+++ CitrsLite.Business/Services/EmailService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CitrsLite.Business.Services
+{
+    public class EmailService
+    {
+        public async Task Email(int participantId, string template)
+        {
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.IsBodyHtml= true;
+        }
+    }
+}
