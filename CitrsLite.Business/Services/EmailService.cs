81915f7ea393896fab5b899833fc4fb48d87bdc6
Daniel Greco <Daniel.Greco@fdacs.gov>
2023-02-06 15:23:48 -0500

    Completed the Email Method on the Email Service Class.

--- CitrsLite.Business/Services/EmailService.cs
+++ CitrsLite.Business/Services/EmailService.cs
@@ -13,6 +13,22 @@ namespace CitrsLite.Business.Services
         {
             MailMessage mailMessage = new MailMessage();
             mailMessage.IsBodyHtml= true;
+            mailMessage.Body = template;
+            mailMessage.To.Add(new MailAddress("Daniel.Greco@fdacs.gov"));
+            mailMessage.From = (new MailAddress("Citrs@fdacs.gov", "Citrs"));
+            mailMessage.Subject = "Testing Attachment";            
+
+            SmtpClient client = new SmtpClient("relay.freshfromflorida.com", 25);
+
+            try
+            {
+                client.Send(mailMessage);
+
+            }
+            catch (Exception exception)
+            {
+                throw;
+            }
         }
     }
 }
