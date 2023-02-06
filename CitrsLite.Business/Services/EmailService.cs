5281369cc65e5a81ff97e5b6c779deaee6ce8336
Daniel Greco <Daniel.Greco@fdacs.gov>
2023-02-06 16:23:22 -0500

    Made further changes to the email functionality.

--- CitrsLite.Business/Services/EmailService.cs
+++ CitrsLite.Business/Services/EmailService.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using iText.Html2pdf;
+using iText.Kernel.Pdf;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net.Mail;
 using System.Text;
@@ -17,22 +21,60 @@ namespace CitrsLite.Business.Services
 
         public async Task EmailAsync(int participantId, string path)
         {
-            var template = await _participantService.GetTemplateAsync(participantId, path);
-            
-            MailMessage mailMessage = new MailMessage();
-            mailMessage.IsBodyHtml= true;
-            mailMessage.Body = template;
-            mailMessage.To.Add(new MailAddress("Daniel.Greco@fdacs.gov"));
-            mailMessage.From = (new MailAddress("Citrs@fdacs.gov", "Citrs"));
-            mailMessage.Subject = "Testing Attachment";            
-
-            SmtpClient client = new SmtpClient("relay.freshfromflorida.com", 25);
+            try
+            {
+                var template = await _participantService.GetTemplateAsync(participantId, path);
 
 
-            await client.SendMailAsync(mailMessage);
-            
 
-            
+                MailMessage mailMessage = new MailMessage();
+                mailMessage.IsBodyHtml = true;
+                mailMessage.Body = template;
+                mailMessage.To.Add(new MailAddress("Daniel.Greco@fdacs.gov"));
+                mailMessage.From = (new MailAddress("Citrs@fdacs.gov", "Citrs"));
+                mailMessage.Subject = "Testing Attachment";
+                
+
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    
+                    await Task.Run(() =>
+                    {
+                        using (PdfWriter pdfWriter = new PdfWriter(stream))
+                        {
+                            stream.Position = 0;
+                            pdfWriter.IsCloseStream();
+                            HtmlConverter.ConvertToPdf(template, pdfWriter);
+                            pdfWriter.SetCloseStream(false);
+
+
+                                                        
+                            mailMessage.Attachments.Add(new Attachment(stream, "participant.pdf"));
+
+
+
+                            
+
+
+                        }
+
+                    });
+                    SmtpClient client = new SmtpClient("relay.freshfromflorida.com", 25);
+                    await client.SendMailAsync(mailMessage);
+                }                                         
+
+                
+
+
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
         }
+
+        
     }
 }
